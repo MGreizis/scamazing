82ffffeb9cd69eb1658a7363b044cdf13e6fe447
bashirHZ <ahme0011@hz.nl>
2022-01-17 11:32:54 +0100

    fixed tiny door position issue

--- src/Doors.ts
+++ src/Doors.ts
@@ -9,8 +9,8 @@ export default abstract class Doors extends GameItem {
    */
   public constructor(maxX:number, maxY:number, img: string) {
     super(img);
-    this.xPos = Utils.randomNumber(0, maxX - (this.img.width * 2));
-    this.yPos = Utils.randomNumber(0, maxY - (this.img.height * 2));
+    this.xPos = Utils.randomNumber(0 + this.img.width, maxX - (this.img.width));
+    this.yPos = Utils.randomNumber(0 + this.img.height, maxY - (this.img.height));
 
     this.maxX = maxX;
     this.maxY = maxY;
